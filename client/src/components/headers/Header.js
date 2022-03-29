17f2576e477d3d3a9b71838d7a4346d1a479ac8a
yoralong <yoralong@gmail.com>
2022-03-29 08:18:31 +0700

    make it better

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -52,7 +52,7 @@ function Header() {
 
             <div className="logo">
                 <h1>
-                    <Link to="/">{isAdmin ? 'Admin' : 'DevAT Shop'}</Link>
+                    <Link to="/">{isAdmin ? 'Admin' : 'ShopSecondHand'}</Link>
                 </h1>
             </div>
 
