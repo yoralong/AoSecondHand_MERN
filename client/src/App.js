dac6f32ff5b49d6005bcce279162d4068b311345
yoralong <yoralong@gmail.com>
2022-04-04 00:03:55 +0700

    add messenger

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalState'
 import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
 function App() {
@@ -13,6 +13,9 @@ function App() {
         <div className="App">
           <Header />
           <MainPages />
+          <MessengerCustomerChat
+          pageId="110672616981089"
+          appId="638516363904741"/>
         </div>
       </Router>
     </DataProvider>
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 
 ReactDOM.render(<App />, 
-<MessengerCustomerChat
-    pageId="110672616981089"
-    appId="638516363904741"
-    
-  />,
   document.getElementById('root')
 );
 
